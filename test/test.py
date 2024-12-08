b09753dcea90ddf07f32e1462a948fbc08c10060
rej <rzioma@acm.org>
2024-12-08 13:30:03 +0100

    Refactoring test_popcount()

--- test/test.py
+++ test/test.py
@@ -26,6 +26,51 @@ def get_output(dut):
         value -= 0x10000
     return value
 
+async def test_popcount(dut, popcount, bits):
+    def population_count(n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
+    def set_every_2nd_bit(n):
+        v = 0
+        for k in range(bits//2):
+            v = (v << 2) | 1
+        return v
+    def set_rightmost_bits(n):
+        return (1 << n) - 1 # Generate a bitmask with the N rightmost bits set
+
+    dut._log.info("Check low 16 bit")
+    for i in range(0xFFFF):
+        popcount.data.value = i
+        await ClockCycles(dut.clk, 1)
+        assert popcount.count.value == population_count(i)
+
+    dut._log.info("Checker patterns")
+    v = set_every_2nd_bit(bits)
+    for i in range(bits):
+        popcount.data.value = v
+        await ClockCycles(dut.clk, 1)
+        assert popcount.count.value == population_count(v)
+        v >>= 1
+
+    v = set_every_2nd_bit(bits)
+    for i in range(bits):
+        popcount.data.value = v
+        await ClockCycles(dut.clk, 1)
+        assert popcount.count.value == population_count(v)
+        v &= ~(1 << i)
+
+    dut._log.info("Fill pattern")
+    for j in range(bits-1):
+        v = set_rightmost_bits(j)
+        for i in range(bits):
+            popcount.data.value = v
+            await ClockCycles(dut.clk, 1)
+            assert popcount.count.value == population_count(v)
+            v >>= 1
+
 @cocotb.test()
 async def test_pop32(dut):
     if os.getenv('GATES'):
@@ -33,51 +78,24 @@ async def test_pop32(dut):
         return
 
     dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
     await ClockCycles(dut.clk, 1)
 
-    def population_count(n: int) -> int:
-        # Use bit manipulation to count 1s
-        count = 0
-        while n:
-            count += n & 1  # Add the least significant bit
-            n >>= 1         # Shift right to process the next bit
-        return count
+    await test_popcount(dut, dut.test_pop32, 32)
 
-    dut._log.info("Check low 16 bit")
-    for i in range(0xFFFF):
-        dut.test_pop32.data.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.test_pop32.count.value == population_count(i)
+@cocotb.test()
+async def test_pop128(dut):
+    if os.getenv('GATES'):
+        dut._log.info("Skipping in GATE mode")
+        return
 
-    dut._log.info("Checker patterns")
-    i = 0xAAAA_AAAA
-    for j in range(32):
-        dut.test_pop32.data.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.test_pop32.count.value == population_count(i)
-        i >>= 1
+    dut._log.info("Start")
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+    await ClockCycles(dut.clk, 1)
 
-    i = 0xAAAA_AAAA
-    for j in range(32):
-        i &= 0xFFFFFFFF
-        dut.test_pop32.data.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.test_pop32.count.value == population_count(i)
-        i <<= 1
-
-    dut._log.info("Fill pattern")
-    for k in range(31):
-        i = (1 << k) - 1
-        for j in range(32):
-            i &= 0xFFFFFFFF
-            dut.test_pop32.data.value = i
-            await ClockCycles(dut.clk, 1)
-            assert dut.test_pop32.count.value == population_count(i)
-            i <<= 1
+    # sawait test_popcount(dut.test_pop128, 128)
 
 @cocotb.test()
 async def test_project(dut):
